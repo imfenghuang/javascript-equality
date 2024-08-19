bc7f15fa73512876e42644e33ec15b1bd04031a9
imfenghuang <20472384+imfenghuang@users.noreply.github.com>
2024-08-19 22:43:16 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -6,24 +6,38 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    AutoImport({
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue$/,
-        /\.vue\?vue/, // .vue
-        /\.md$/, // .md
-      ],
-      imports: ['vue', '@vueuse/core'],
-      // dts: true
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ mode }) => {
+  let config = {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      AutoImport({
+        include: [
+          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+          /\.vue$/,
+          /\.vue\?vue/, // .vue
+          /\.md$/, // .md
+        ],
+        imports: ['vue', '@vueuse/core'],
+        // dts: true
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
   }
+
+  // for pages
+  if (mode === "pages") {
+    config = Object.assign({}, config, {
+      build: {
+        outDir: 'docs'
+      },
+      base: './',
+    })
+  }
+
+  return config
 })
